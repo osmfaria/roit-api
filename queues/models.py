a7a0925b7baa32465da77e61c9ac5396d58054eb
Osmar Faria <osm.faria@gmail.com>
2023-05-11 10:35:01 -0700

    feat: add queue model

--- queues/models.py
+++ queues/models.py
@@ -1,3 +1,17 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Queue(models.Model):
+    id = models.UUIDField(default=uuid, primary_key=True, editable=False)
+    section_code = models.CharField(max_length=5)
+    section_name = models.TextField()
+    division_code = models.CharField(max_length=5)
+    division_name = models.TextField()
+    group_code = models.CharField(max_length=10)
+    group_name = models.TextField()
+    class_code = models.CharField(max_length=10)
+    class_name = models.TextField()
+    subclass_code = models.CharField(max_length=15)
+    subclass_name = models.TextField()
