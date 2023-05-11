7675c08db7ddb0a88ec6ebaebde3f92207c4827e
Osmar Faria <osm.faria@gmail.com>
2023-05-11 10:59:03 -0700

    feat: add queue views

--- queues/views.py
+++ queues/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
 from .models import Queue
+from .serializers import QueueSerializer
 
 class QueueView(generics.ListCreateAPIView):
-    queryset = Queue
+    queryset = Queue.objects.all()
+    serializer_class = QueueSerializer 
