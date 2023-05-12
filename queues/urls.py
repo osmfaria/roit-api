4fb90f668167632154af00d760a0684f921d086c
Osmar Faria <osm.faria@gmail.com>
2023-05-12 11:39:02 -0700

    feat: add filters for queue view

--- queues/urls.py
+++ queues/urls.py
@@ -4,8 +4,9 @@ from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 from . import views
 
 urlpatterns = [
-    path('cnab/', views.QueueView.as_view(), name="queue-view"),
-    path('cnab/remove/', views.QueueDeleteView.as_view(), name="queue_delete-view"),
+    path('cnae/', views.QueueView.as_view(), name="queue-view"),
+    path('cnae/remove/', views.QueueDeleteView.as_view(), name="queue_delete-view"),
+    path('cnae/filter/', views.QueueFilterView.as_view(), name="queue_filter-view"),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- queues/views.py
+++ queues/views.py
@@ -1,12 +1,15 @@
+from django_filters import rest_framework as filters
 from rest_framework import generics
 from rest_framework.views import Response, status
 
 from .models import Queue
 from .serializers import QueueSerializer
 
+
 class QueueView(generics.ListCreateAPIView):
     queryset = Queue.objects.all()
     serializer_class = QueueSerializer
+
 
 class QueueDeleteView(generics.DestroyAPIView):
     queryset = Queue.objects.all()
@@ -15,3 +18,47 @@ class QueueDeleteView(generics.DestroyAPIView):
     def destroy(self, request, *args, **kwargs):
         self.queryset.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class QueueFilter(filters.FilterSet):
+    section_code = filters.CharFilter(
+        field_name="section_code", lookup_expr="icontains"
+    )
+    section_name = filters.CharFilter(
+        field_name="section_name", lookup_expr="icontains"
+    )
+    division_code = filters.CharFilter(
+        field_name="division_code", lookup_expr="icontains"
+    )
+    division_name = filters.CharFilter(
+        field_name="division_name", lookup_expr="icontains"
+    )
+    class_code = filters.CharFilter(field_name="class_code", lookup_expr="icontains")
+    class_name = filters.CharFilter(field_name="class_name", lookup_expr="icontains")
+
+    class Meta:
+        model = Queue
+        fields = [
+            "section_code",
+            "section_name",
+            "division_code",
+            "division_name",
+            "class_code",
+            "class_name",
+        ]
+
+
+class QueueFilterView(generics.ListAPIView):
+    queryset = Queue.objects.all()
+    serializer_class = QueueSerializer
+
+    lookup_url_kwarg = [
+        "section_code",
+        "section_name",
+        "division_code",
+        "division_name",
+        "class_code",
+        "class_name",
+    ]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = QueueFilter
